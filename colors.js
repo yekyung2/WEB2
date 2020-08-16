d0bcaa25ba2085266e6c0cb28aed8cabee270d03
deepdiver <skybluee2014@gmail.com>
2020-08-17 01:46:28 +0900

    Add Jquery to colors.js

--- colors.js
+++ colors.js
@@ -1,20 +1,24 @@
 /*나이트-데이모드 전환시 바디 텍스트 & 바디 백그리운드 컬러 전환 함수*/
 var Body = {
     setColor:function (color){
-    document.querySelector('body').style.color = color;},
+    // document.querySelector('body').style.color = color;},
+    $('body').css('color',color);
+    },
     setBackgroundColor:function (color){
-   document.querySelector('body').style.backgroundColor = color;
-   }
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
+    }
  }
   
  /* 나이트-데이모드 전환시 링크 컬러 전환 함수. 함수 실행 시, 변인에 원하는 색상 입력*/
  var Links = {
    setColor:function(color){
-     var alist = document.querySelectorAll('a');
-     var i = 0;
-     while(i<alist.length){
-       alist[i].style.color = color;
-       i = i+1;}
+        //      var alist = document.querySelectorAll('a');
+        //      var i = 0;
+        //      while(i<alist.length){
+        //        alist[i].style.color = color;
+        //        i = i+1;}
+    $('a').css('color',color);
    }
  }
  
